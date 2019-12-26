edb342d36b8e5a82908a94477417e7b3946087c6
Andrei Radoi <radoiandrei37@gmail.com>
2019-12-26 18:40:46 +0200

    Removed node.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 80
-
-app.use('/static', express.static('static'))
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
-
-app.listen(port, () => console.log(`Reveu app listening on port ${port}!`))
